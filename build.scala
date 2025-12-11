01811681dd1620cfc8a571d7e4090d764da1d9f7
Andrew Valencik <andrew.valencik@shopify.com>
2025-12-11 09:12:30 -0500

    Breakup build script into Build, Server, Main

--- build.scala
+++ build.scala
@@ -2,35 +2,22 @@
 //> using dep org.typelevel::laika-io:1.3.2
 //> using dep org.typelevel::cats-effect:3.6.3
 //> using dep org.http4s::http4s-ember-server:0.23.33
-//> using dep org.http4s::http4s-dsl:0.23.33
 //> using dep ch.qos.logback:logback-classic:1.5.21
 //> using resourceDir resources
 
 package tlsite
 
-import cats.effect.{IO, IOApp, ExitCode}
-import laika.api._
-import laika.format._
-import laika.io.syntax._
-import laika.io.model.FilePath
-import laika.theme.Theme
-import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.Router
-import org.http4s.server.middleware.Logger as RequestLogger
-import org.http4s.server.staticcontent._
-import org.typelevel.log4cats.LoggerFactory
-import org.typelevel.log4cats.slf4j.Slf4jFactory
-import com.comcast.ip4s._
-import fs2.io.file.Path
+import cats.effect.IO
 
-object Build extends IOApp {
+object Build {
+  import laika.api._
+  import laika.format._
+  import laika.io.syntax._
+  import laika.theme.Theme
 
-  given LoggerFactory[IO] = Slf4jFactory.create[IO]
-  private val logger = LoggerFactory[IO].getLogger
+  val outputDir = "target/site"
 
-  val siteDir = "target/site"
-
-  def build: IO[Unit] = {
+  def run: IO[Unit] = {
     val transformer = Transformer
       .from(Markdown)
       .to(HTML)
@@ -39,29 +26,24 @@ object Build extends IOApp {
       .withTheme(Theme.empty)
       .build
 
-    transformer.use { t =>
-      t.fromDirectory("src")
-        .toDirectory(siteDir)
-        .transform
-    }.void
+    transformer.use_(_.fromDirectory("src").toDirectory(outputDir).transform)
   }
+}
 
-  def run(args: List[String]): IO[ExitCode] = {
-    val shouldServe = args.contains("--serve") || args.contains("-s")
-    build >> (if (shouldServe) serve(parsePort(args)) else IO.unit).as(ExitCode.Success)
-  }
+object Server {
+  import org.http4s.ember.server.EmberServerBuilder
+  import org.http4s.server.Router
+  import org.http4s.server.middleware.Logger as RequestLogger
+  import org.http4s.server.staticcontent._
+  import org.typelevel.log4cats.LoggerFactory
+  import org.typelevel.log4cats.slf4j.Slf4jFactory
+  import com.comcast.ip4s._
 
-  private def parsePort(args: List[String]): Port = {
-    val portIdx = args.indexWhere(a => a == "--port" || a == "-p")
-    val parsed = for {
-      idx <- Option.when(portIdx >= 0 && portIdx + 1 < args.length)(portIdx)
-      port <- Port.fromString(args(idx + 1))
-    } yield port
-    parsed.getOrElse(port"8080")
-  }
+  given LoggerFactory[IO] = Slf4jFactory.create[IO]
+  private val logger = LoggerFactory[IO].getLogger
 
-  def serve(port: Port): IO[Unit] = {
-    val routes = Router("/" -> fileService[IO](FileService.Config(siteDir))).orNotFound
+  def run(port: Port): IO[Unit] = {
+    val routes = Router("/" -> fileService[IO](FileService.Config(Build.outputDir))).orNotFound
     val app = RequestLogger.httpApp(logHeaders = false, logBody = false)(routes)
 
     logger.info(s"Serving site at http://localhost:$port") >>
@@ -72,5 +54,24 @@ object Build extends IOApp {
       .withHttpApp(app)
       .build
       .useForever
+  }
+}
+
+object Main extends cats.effect.IOApp {
+  import cats.effect.ExitCode
+  import com.comcast.ip4s._
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val shouldServe = args.contains("--serve") || args.contains("-s")
+    Build.run >> (if (shouldServe) Server.run(parsePort(args)) else IO.unit).as(ExitCode.Success)
+  }
+
+  private def parsePort(args: List[String]): Port = {
+    val portIdx = args.indexWhere(a => a == "--port" || a == "-p")
+    val parsed = for {
+      idx <- Option.when(portIdx >= 0 && portIdx + 1 < args.length)(portIdx)
+      port <- Port.fromString(args(idx + 1))
+    } yield port
+    parsed.getOrElse(port"8080")
   }
 }
